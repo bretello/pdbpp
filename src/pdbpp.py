e1325f35dc79af7c5b48e05d01d1af5c11926207
bretello <bretello@distruzione.org>
2023-07-08 00:29:32 +0200

    fix python -m pdb on 3.11
    based on the changes in proposed in #516 and #520
    
    fixes #516

--- src/pdbpp.py
+++ src/pdbpp.py
@@ -92,7 +92,13 @@ def rebind_globals(func, newglobals):
             _newfunc(func.func, newglobals), *func.args, **func.keywords
         )
 
-    raise ValueError("cannot handle func {!r}".format(func))
+    if sys.version_info >= (3, 11) and func.__name__ in (
+        "_ModuleTarget",
+        "_ScriptTarget",
+    ):
+        return func
+
+    raise ValueError(f"cannot handle func {func}")
 
 
 class DefaultConfig:
@@ -2154,10 +2160,13 @@ if hasattr(pdb, "_usage"):
     _usage = pdb._usage
 
 # copy some functions from pdb.py, but rebind the global dictionary
-for name in "run runeval runctx runcall main set_trace".split():
+to_rebind = ["run", "runeval", "runctx", "runcall", "main", "set_trace"]
+if sys.version_info >= (3, 11):
+    to_rebind += ["_ModuleTarget", "_ScriptTarget"]
+for name in to_rebind:
     func = getattr(pdb, name)
     globals()[name] = rebind_globals(func, globals())
-del name, func
+del name, func, to_rebind
 
 
 # Post-Mortem interface
